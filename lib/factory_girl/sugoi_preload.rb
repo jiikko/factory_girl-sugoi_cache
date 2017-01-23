359aed7b209db5eef1468eefe54edb25c74d60c3
jiikko <n905i.1214@gmail.com>
2017-01-23 22:37:02 +0900

    first commit

---
+++ lib/factory_girl/sugoi_preload.rb
@@ -0,0 +1,7 @@
+require "factory_girl/sugoi_preload/version"
+
+module FactoryGirl
+  module SugoiPreload
+    # Your code goes here...
+  end
+end
---
+++ lib/factory_girl/sugoi_preload/version.rb
@@ -0,0 +1,5 @@
+module FactoryGirl
+  module SugoiPreload
+    VERSION = "0.1.0"
+  end
+end
